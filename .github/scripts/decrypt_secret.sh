34e370f2574782b731e131baf8f43a661939084a
Mike Cuoco <mcuoco12@gmail.com>
2020-08-11 15:33:56 -0400

    remove variables for now

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,11 +3,11 @@
 # Decrypt the file
 mkdir $HOME/secrets
 
-ENC_TOKEN=$(ls -1 ./.secrets)
+#ENC_TOKEN=$(ls -1 ./.secrets)
 #ENC_TOKEN=$(ls -1 ~/cv/.secrets)
-DEC_TOKEN=${ENC_TOKEN::${#ENC_TOKEN}-4}
+#DEC_TOKEN=${ENC_TOKEN::${#ENC_TOKEN}-4}
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/$DEC_TOKEN ./.secrets/$ENC_TOKEN
+--output $HOME/secrets/6c91f3dd95c2217959d38f926b96d7bb_mcuoco12@gmail.com ./.secrets/6c91f3dd95c2217959d38f926b96d7bb_mcuoco12@gmail.com.gpg
