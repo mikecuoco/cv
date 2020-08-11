7c99231b76d2290f21ff723217a4b19a3fdcd2f6
Mike Cuoco <mcuoco12@gmail.com>
2020-08-11 11:01:26 -0400

    update path to encrypted secret

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/6c91f3dd95c2217959d38f926b96d7bb_mcuoco12@gmail.com $HOME/.secrets/6c91f3dd95c2217959d38f926b96d7bb_mcuoco12@gmail.com.gpg
+--output $HOME/secrets/6c91f3dd95c2217959d38f926b96d7bb_mcuoco12@gmail.com ./.secrets/6c91f3dd95c2217959d38f926b96d7bb_mcuoco12@gmail.com.gpg
